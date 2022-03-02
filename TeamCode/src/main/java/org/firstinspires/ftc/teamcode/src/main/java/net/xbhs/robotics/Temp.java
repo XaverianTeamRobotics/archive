24eb4a97bbede944213a16223fa2a955d169acc0
Thomas Ricci <git@thomasricci.dev>
2022-03-02 11:43:16 -0500

    Temp packages

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/src/main/java/net/xbhs/robotics/Temp.java
@@ -0,0 +1,5 @@
+package org.firstinspires.ftc.teamcode.src.main.java.net.xbhs.robotics;
+
+// DO NOT REMOVE -- TEMPORARILY REQUIRED FOR GIT TRACKING
+public class Temp {
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/src/test/java/net/xbhs/robotics/Temp.java
@@ -0,0 +1,5 @@
+package org.firstinspires.ftc.teamcode.src.test.java.net.xbhs.robotics;
+
+// DO NOT REMOVE -- TEMPORARILY REQUIRED FOR GIT TRACKING
+public class Temp {
+}
